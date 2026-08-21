6271261ce408eaf6a0a78ee5af59e43f586b8e50
berzrk1 <67083456+berzrk1@users.noreply.github.com>
2026-08-20 22:34:04 -0300

    fix(fish): ls

--- dot_config/private_fish/functions/ls.fish
+++ dot_config/private_fish/functions/ls.fish
@@ -1,6 +1,6 @@
 function ls --wraps='eza --icons --git --group-directories-first' --description 'alias ls=eza --icons --git --group-directories-first'
     if type -f eza &>/dev/null
-        eza --icons --git --group-directories-first --hyperlink $argv
+        eza --icons --git --group-directories-first --hyperlink=auto $argv
     else
         missing_package eza
     end
