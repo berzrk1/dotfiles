1e86c1bcda32614a3b4a3c45b0f2d678bd2a068f
berzrk1 <67083456+berzrk1@users.noreply.github.com>
2026-09-10 00:32:35 -0300

    fix lt alias and change tmux prefix to c-space

--- dot_config/private_fish/functions/lt.fish
+++ dot_config/private_fish/functions/lt.fish
@@ -1,6 +1,6 @@
 function lt --wraps='eza --long --header --icons --tree --git --group-directories-first' --description 'alias lt=eza --long --header --icons --tree --git --group-directories-first'
     if type -f eza &>/dev/null
-        eza --long --header --icons --tree --git --group-directories-first --hyperlink $argv
+        eza --long --header --icons --tree --git --group-directories-first --hyperlink=auto $argv
     else
         missing_package eza
     end
