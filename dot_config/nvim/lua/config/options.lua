94db16687728d498e18b97c49d30373e6b6abaef
berzrk1 <67083456+berzrk1@users.noreply.github.com>
2026-09-10 11:38:43 -0300

    nvim: add neovide settings and remove image plugin

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -6,3 +6,12 @@
 vim.g.lazyvim_check_order = false
 
 vim.g.lazyvim_python_ruff = "ruff"
+
+if vim.g.neovide then
+  vim.g.neovide_cursor_animation_length = 0
+  vim.g.neovide_cursor_short_animation_length = 0
+  vim.g.neovide_cursor_trail_size = 0
+  vim.g.neovide_cursor_animate_in_insert_mode = false
+  vim.g.neovide_cursor_animate_command_line = false
+  vim.g.neovide_cursor_vfx_mode = ""
+end
